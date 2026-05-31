5129c484c9d3a5087ebe7569eebc8bd5eab4e2a7
lucagmb <lucagmb@users.noreply.github.com>
2026-05-30 23:03:40 -0300

    feat(config): rewrite Entorno with full config management
    Add fields: moneda, sfx, rdr, idModelo, estiloGrafica, volumenMusica.
    Auto-load from config.json on construction. Add guardarConfiguracion()
    with safeguard against overwriting uninitialized state. Add
    enviarNConfig() for bulk updates.

--- src/NTI/Entorno.java
+++ src/NTI/Entorno.java
@@ -1,26 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package NTI;
 
-/**
- *
- * @author NICO
- */
 public class Entorno {
-    
-    Registro reg = new Registro();
-    Lectura lec = new Lectura();
-    
-    public boolean enviarNConfig(String mon, boolean sfx, boolean rdr, int idModelo, String estiloGrafica){
-        boolean v = reg.actualizarCofig(mon, sfx, rdr, idModelo, estiloGrafica); 
-        return(v);
+
+    public String moneda;
+    public boolean sfx;
+    public boolean rdr;
+    public int idModelo;
+    public String estiloGrafica;
+    public int volumenMusica;
+
+    public boolean isLoaded = false;
+
+    private Registro reg;
+    private Lectura lec;
+
+    public Entorno() {
+        this.reg = new Registro();
+        this.lec = new Lectura();
+        cargarConfiguracionDesdeJSON();
     }
-    
-    public String[] conseguirConfig(){
-        String[] valores = lec.obtenerConfig();
-        return(valores);
+
+    public void cargarConfiguracionDesdeJSON() {
+        String[] valores = lec.obtenerConfig(); 
+        
+        this.moneda = valores[0];
+        this.sfx = Boolean.parseBoolean(valores[1]);
+        this.rdr = Boolean.parseBoolean(valores[2]);
+        this.idModelo = Integer.parseInt(valores[3]);
+        this.estiloGrafica = valores[4];
+        this.volumenMusica = Integer.parseInt(valores[5]);
+        this.isLoaded = true;
+    }
+
+    public boolean guardarConfiguracion() {
+        if (!this.isLoaded) {
+            System.err.println("ADVERTENCIA: Se intentó guardar un objeto Entorno no inicializado. Forzando recarga desde config.json para prevenir sobreescritura.");
+            cargarConfiguracionDesdeJSON();
+        }
+        boolean v = reg.actualizarCofig(
+            this.moneda, 
+            this.sfx, 
+            this.rdr, 
+            this.idModelo, 
+            this.estiloGrafica,
+            this.volumenMusica 
+        );
+        return v;
+    }
+
+    public boolean enviarNConfig(String mon, boolean sfx, boolean rdr, int idModelo, String estiloGrafica, int volumen) {
+        this.moneda = mon;
+        this.sfx = sfx;
+        this.rdr = rdr;
+        this.idModelo = idModelo;
+        this.estiloGrafica = estiloGrafica;
+        this.volumenMusica = volumen;
+
+        return guardarConfiguracion();
+    }
+
+    public String[] conseguirConfig() {
+        return new String[] {
+            this.moneda,
+            String.valueOf(this.sfx),
+            String.valueOf(this.rdr),
+            String.valueOf(this.idModelo),
+            this.estiloGrafica,
+            String.valueOf(this.volumenMusica)
+        };
     }
 }
